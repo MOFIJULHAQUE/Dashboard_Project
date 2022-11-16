d3f9e2762119aa317a12f5a39d14bfa47c2259e3
Rajesh Saini <81741413+saini9485@users.noreply.github.com>
2022-11-17 02:49:29 +0530

    In  topCoins file loader added and some changes

--- src/components/topCoins/TopCoins.jsx
+++ src/components/topCoins/TopCoins.jsx
@@ -7,43 +7,52 @@ import "./TopCoins.css";
 
 export function TopCoins() {
   const [topCoin, setTopCoin] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios
-      .get(
+    const Crypto = async () => {
+      setLoading(true);
+      const response = await axios.get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false"
-      )
-      .then((response) => {
-        setTopCoin([...response.data]);
-      });
+      );
+      setTopCoin(response.data);
+      setLoading(false);
+    };
+    Crypto();
   }, []);
 
   return (
     <div className="top_coin_card">
-      {topCoin.map((coin, i) => {
-        const {
-          image,
-          symbol,
-          price_change_percentage_24h,
-          price_change_24h,
-          total_volume,
-          current_price,
-        } = coin;
-        if (i < 3) {
-          return (
-            <>
-              <TopCoinsData
-                image={image}
-                symbol={symbol}
-                price_change_percentage_24h={price_change_percentage_24h}
-                price_change_24h={price_change_24h}
-                total_volume={total_volume}
-                current_price={current_price}
-              />
-            </>
-          );
-        }
-      })}
+      {loading ? (
+        <h1 className="Loader">
+          <i className="fa fa-refresh fa-spin"></i>Loading...
+        </h1>
+      ) : (
+        topCoin.map((coin, i) => {
+          const {
+            image,
+            symbol,
+            price_change_percentage_24h,
+            price_change_24h,
+            total_volume,
+            current_price,
+          } = coin;
+          if (i < 3) {
+            return (
+              <>
+                <TopCoinsData
+                  image={image}
+                  symbol={symbol}
+                  price_change_percentage_24h={price_change_percentage_24h}
+                  price_change_24h={price_change_24h}
+                  total_volume={total_volume}
+                  current_price={current_price}
+                />
+              </>
+            );
+          }
+        })
+      )}
       <div className="all_coins coin_box">
         <img className="all_coins_logo" src="Assets/Coins2.jpg" alt="coins" />
         <h3 className="ExploreCoin">Crypto Exchange</h3>
